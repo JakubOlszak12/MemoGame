653368a332440a98ddffdd17c93e85e1cbc52afb
Jakub <j.jakub66@gmail.com>
2024-04-27 17:08:40 +0200

    Initial Commit

---
+++ swiftmemogameJO/swiftmemogameJOApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftmemogameJOApp.swift
+//  swiftmemogameJO
+//
+//  Created by student on 27/4/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftmemogameJOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
