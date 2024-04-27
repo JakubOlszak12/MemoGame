8239a047499cb1010df325a016598da69111ddde
Jakub <j.jakub66@gmail.com>
2024-04-27 18:31:22 +0200

    Finished lab2

---
+++ swiftmemogameJO/CardView.swift
@@ -0,0 +1,8 @@
+//
+//  CardView.swift
+//  swiftmemogameJO
+//
+//  Created by student on 27/4/24.
+//
+
+import Foundation
---
+++ swiftmemogameJO/Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Theme.swift
+//  swiftmemogameJO
+//
+//  Created by student on 27/4/24.
+//
+
+import Foundation
